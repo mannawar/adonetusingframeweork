b9fdcdd3fd3e8e92020ce0b7c11def58191c44a8
mannawar <dearmannawar@gmail.com>
2022-12-30 05:52:00 +0300

    Prt 8 sqlDataReader adonet series

--- adonetusingframeweork/Default.aspx
+++ adonetusingframeweork/Default.aspx
@@ -1,6 +1,7 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="adonetusingframeweork._Default" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView ID="GridView2" runat="server"></asp:GridView>
 
     <asp:GridView ID="GridView1" runat="server"></asp:GridView>
 
--- adonetusingframeweork/Default.aspx.cs
+++ adonetusingframeweork/Default.aspx.cs
@@ -18,11 +18,33 @@ namespace adonetusingframeweork
             string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
             using (SqlConnection con = new SqlConnection(CS))
             {
-                SqlCommand cmd = new SqlCommand("select * from AspNetUsers", con);
+                //SqlCommand cmd = new SqlCommand("select * from AspNetUsers", con);
+                //con.Open();
+                //SqlDataReader rdr = cmd.ExecuteReader();
+                //GridView1.DataSource = rdr;
+                //GridView1.DataBind();
+
+                //SqlCommand cmd = new SqlCommand("select Count(UserName) from AspNetUsers", con);
+                //con.Open();
+                //int TotalCount = (int)cmd.ExecuteScalar();
+                //Response.Write("Total Rows="+ TotalCount.ToString());
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
                 con.Open();
-                SqlDataReader rdr = cmd.ExecuteReader();
-                GridView1.DataSource = rdr;
-                GridView1.DataBind();
+
+                cmd.CommandText = "Delete from tblProducts where Id =5";
+                int TotalRowsDeleted = cmd.ExecuteNonQuery();
+                Response.Write("Total Rows Deleted=" + TotalRowsDeleted.ToString() + "<br/>");
+
+                cmd.CommandText = "Insert into tblProducts Values(4, 'Tabs', 300, 1000)";
+                int TotalRowsAdded = cmd.ExecuteNonQuery();
+                Response.Write("Total Rows Added=" + TotalRowsAdded.ToString() + "<br/>");
+
+                cmd.CommandText = "update tblProducts set unitPrice=1800 where Id = 2";
+                int TotalRowsUpdated = cmd.ExecuteNonQuery();
+                Response.Write("Total Rows Added=" + TotalRowsUpdated.ToString() + "<br/>");
+
             }
             //OracleConnection con = new OracleConnection("Data Source=(localdb)\\MSSQLLocalDB;Database=EmployeeDB;Integrated Security=True;");
             //OracleCommand cmd = new OracleCommand("select * from AspNetUsers", con);
